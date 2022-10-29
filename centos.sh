4c4fe50821645a96499b1c852bf1354d4e7a7cbc
mmirene86@yahoo.com <mmirene86@yahoo.com>
2022-10-28 23:45:53 -0400

    Docker installation on centos

---
+++ centos.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Author: Mirene Mbida
+#Description: installation of docker in centos"
+
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo systemctl start docker
+sudo docker run hello-world
+
+echo "Docker has successfully being installed"
